21e5989f65623ed08b19fc042119c59b7eaa8c82
sungsan <kunga077@gmail.com>
2021-01-17 21:39:58 +0900

    first commit

---
+++ src/main/java/com/example/studyadmin/StudyadminApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyadminApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StudyadminApplication.class, args);
+  }
+
+}
