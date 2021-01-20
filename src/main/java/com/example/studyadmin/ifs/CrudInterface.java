920e48234f02768d6e2bb8430f751eca84fa8007
sungsan <kunga077@gmail.com>
2021-01-20 17:28:33 +0900

    CRUD Interface 수정 Header 클래스에 Generic Class 추가

--- src/main/java/com/example/studyadmin/ifs/CrudInterface.java
+++ src/main/java/com/example/studyadmin/ifs/CrudInterface.java
@@ -2,13 +2,13 @@ package com.example.studyadmin.ifs;
 
 import com.example.studyadmin.model.network.Header;
 
-public interface CrudInterface {
+public interface CrudInterface<Req, Res> {
 
-  Header create();
+  Header<Res> create(Header<Req> request);
 
-  Header read(Long id);
+  Header<Res> read(Long id);
 
-  Header update();
+  Header<Res> update(Header<Req> request);
 
   Header delete(Long id);
 
